026480e7583f021207826646e744b1b5cc9cd9f5
GitHub Action <action@github.com>
2021-04-30 05:57:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/ananasxxj/hextocolor/MyBundle.kt
+++ src/main/kotlin/com/github/ananasxxj/hextocolor/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ananasxxj.hextocolor
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ananasxxj/hextocolor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ananasxxj/hextocolor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ananasxxj.hextocolor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ananasxxj.hextocolor.MyBundle
 
 class MyApplicationService {
 
